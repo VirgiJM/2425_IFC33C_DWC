41aa34b94f54bfd2e0a14367d788f017b1b0505d
Virginia Jimenez <virginiajimenez@paucasesnovescifp.cat>
2024-11-11 12:51:20 +0100

    Aplicada pequeña modificación de estilos a la tabla.

--- Tema_2/ActividadesDOM2/DWC_ut3_hej_1_DOM/ejercicio6/Ejercicio6.js
+++ Tema_2/ActividadesDOM2/DWC_ut3_hej_1_DOM/ejercicio6/Ejercicio6.js
@@ -5,7 +5,7 @@ const persona3 = { nombre: "Alexander", edad: 38, genero: "Hombre", email: "alex
 const persona4 = { nombre: "David", edad: 15, genero: "Hombre", email: "davidv@hotmail.es" }
 const persona5 = { nombre: "Miriam", edad: 26, genero: "Mujer", email: "mirijm@hotmail.com" }
 
-let arrayObjetos = []
+let arrayObjetos = [];
 arrayObjetos.push(persona1);
 arrayObjetos.push(persona2);
 arrayObjetos.push(persona3);
@@ -14,23 +14,18 @@ arrayObjetos.push(persona5);
 
 console.log(arrayObjetos);
 
-// for (let persona of arrayObjetos) {
-//     console.log(persona.nombre);
-// }
-
-
 const table = document.createElement("table"); // Creamos la tabla.
 table.id = "personas";
-
 document.body.appendChild(table);
 
 table.style.border = "1px solid black";
+table.style.borderCollapse = "collapse"; // Con esto podré hacer que las celdas estén separadas entre sí y no queden juntas.
 
 const thead = document.createElement("thead");
 table.appendChild(thead);
 
 const tr = document.createElement("tr");
-table.appendChild(tr);
+thead.appendChild(tr);
 
 let valores = [];
 let contador = 0;
@@ -40,20 +35,19 @@ for (let llave in persona1) {
     contador++;
 }
 
-console.log(valores);
-
 for (let valor in valores) {
     const th = document.createElement("th");
     th.innerText = valores[valor];
+    th.style.border = "1px solid black"; // Borde para las celdas de encabezado
     tr.appendChild(th);
 }
 
-const td = document.createElement("td");
 for (let i = 0; i < arrayObjetos.length; i++) {
     const tr2 = document.createElement("tr");
     for (let llave in arrayObjetos[i]) {
         const td = document.createElement("td");
         td.innerHTML = arrayObjetos[i][llave];
+        td.style.border = "1px solid black"; // Borde para cada celda
         tr2.appendChild(td);
     }
     table.appendChild(tr2);
