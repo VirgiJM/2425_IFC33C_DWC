3c1c363224ed25c63e47f1286c80dda38e71cda8
Virginia Jimenez <virginiajimenez@paucasesnovescifp.cat>
2024-12-07 13:44:14 +0100

    Añadido algún estilo a la tabla.

--- 1er Trimestre/Tema_3/funcFlecha/DWC_ut4_hej_1_funcFlecha/js/Ejercicio4.js
+++ 1er Trimestre/Tema_3/funcFlecha/DWC_ut4_hej_1_funcFlecha/js/Ejercicio4.js
@@ -7,15 +7,18 @@ let personas = [
 
 // Crear tabla
 const tabla = document.createElement("table");
+tabla.style.border = "1px solid black";
 document.body.appendChild(tabla);
 const thead = document.createElement("thead");
 tabla.appendChild(thead);
 let tr = document.createElement("tr");
 thead.appendChild(tr);
 let th = document.createElement("th");
+th.style.border = "1px solid black";
 th.innerHTML = "Nombre";
 tr.appendChild(th);
 th = document.createElement("th");
+th.style.border = "1px solid black";
 th.innerHTML = "Edad";
 tr.appendChild(th);
 
@@ -25,11 +28,13 @@ let rellenarTabla = () => {
     for (let i = 0; i < personas.length; i++) {
         tr = document.createElement("tr");
         td = document.createElement("td");
+        td.style.border = "1px solid black";
         tabla.appendChild(tr);
         td.innerHTML = personas[i].nombre;
         tr.appendChild(td);
         td = document.createElement("td");
         td.innerHTML = personas[i].edad;
+        td.style.border = "1px solid black";
         tr.appendChild(td);
     }
 }
