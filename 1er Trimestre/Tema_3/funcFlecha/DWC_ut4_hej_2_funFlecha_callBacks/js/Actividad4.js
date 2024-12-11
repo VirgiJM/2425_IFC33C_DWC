e93736db4c316bef1c9c370b551f5ea9cbfd2010
Virginia Jimenez <virginiajimenez@paucasesnovescifp.cat>
2024-12-11 12:45:49 +0100

    Añadido el this.

---
+++ 1er Trimestre/Tema_3/funcFlecha/DWC_ut4_hej_2_funFlecha_callBacks/js/Actividad4.js
@@ -0,0 +1,26 @@
+const tareas = [
+    { descripcion: "Lavar los platos", estado: "pendiente" },
+    { descripcion: "Hacer la compra", estado: "completado" },
+    { descripcion: "Estudiar JavaScript", estado: "pendiente" },
+    { descripcion: "Regar las plantas", estado: "completado" },
+    { descripcion: "Preparar la cena", estado: "pendiente" }
+];
+
+console.log(tareas);
+
+let mostrar = (lista) => {
+    const ul = document.createElement("ul");
+    document.body.appendChild(ul);
+    let li;
+    lista.forEach(element => {
+        li = document.createElement("li");
+        li.innerHTML = `${element.descripcion}`;
+        if (element.estado === "completado") {
+            li.style.color = "lightgreen";
+            li.innerHTML = `<del>${element.descripcion}</del>`;
+        }
+        ul.appendChild(li);
+    });
+}
+
+mostrar(tareas);
